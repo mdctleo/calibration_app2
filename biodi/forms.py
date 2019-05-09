30a46bd81bcebc83806b18cdebbfd951718035c7
Michael Chang <michang@bccrc.ca>
2019-05-09 11:53:21 -0700

    statistic form completed

--- biodi/forms.py
+++ biodi/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, FloatField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -7,3 +7,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class powerForm(FlaskForm):
+    effect = FloatField('Effect', validators=[DataRequired()])
+    alpha = SelectField('Alpha', choices=[(0.05, '0.05'), (0.01, '0.01')])
+    power = FloatField('Power', default=0.8, validators=[DataRequired()])
+    test = SelectField('Test', choices=[('A', 'A'), ('B', 'B'), ('C', 'C')])
+    submit2 = SubmitField('Submit')
