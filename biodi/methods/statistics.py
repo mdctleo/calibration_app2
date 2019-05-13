2addfba8610b0cac0bce77678ee3c6c35fd6feee
Michael Chang <michang@bccrc.ca>
2019-05-13 12:42:41 -0700

    finished all the issues fixed #4 fixed #5 fixed #7 fixed #8

--- biodi/methods/statistics.py
+++ biodi/methods/statistics.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect,url_for, redirect, request
 from statsmodels.stats.power import TTestPower, TTestIndPower
-
+import math as m
 
 def powerFunc(effect,alpha,power,nobs,test):
     
@@ -15,25 +15,15 @@ def powerFunc(effect,alpha,power,nobs,test):
     elif test == 'paired':
         analysis = TTestIndPower()
         result = analysis.solve_power(effect_size=effect,nobs=nobs,alpha=alpha,power=power, ratio = 1, alternative='two-sided')
-    
-    # if (effect is None):
-    #     effect = None
-    # if (nobs is None):
-    #     nobs = None
-    # if (power is None):
-    #     power = None
 
     result = str(result)
 
-
-    # result = analysis.solve_power(effect_size=effect,nobs=nobs,alpha=alpha,power=power)
-
     if effect is None:
-        print('The effect size is {}'.format(result))
-        flash("The effect size is " + result)
+        print("\nThe Effect size should be %.2f\n" % result)
+        flash("The Effect size should be %.2f\n" % result)
     elif power is None:
-        print('The power is {}'.format(result))
-        flash("The power is " + result)
+        print('The power is {}'.format(m.ceil(result)))
+        flash("The power is {}".format(m.ceil(result)))
     elif nobs is None:
-        print('The sample size should be {}'.format(result))
-        flash("The sample size should be " + result)
+        print("The sample size should be %.2f\n" % result)
+        flash("The sample size should be %.2f\n" % result)
--- biodi/routes.py
+++ biodi/routes.py
@@ -5,6 +5,7 @@ from flask_login import current_user, login_user,logout_user, login_required
 from biodi.models import User
 # from biodi.methods.statistics import powerFunc
 from statsmodels.stats.power import TTestPower, TTestIndPower
+import math as m
 
 @app_biodi.route('/')
 @app_biodi.route('/index')
@@ -21,46 +22,16 @@ def gc_biodi():
     return render_template('gcbiodi.html',text=text,user=user)
 
 # connecting to power.html
-@app_biodi.route('/power')#, methods=['GET', 'POST'])
+@app_biodi.route('/power')
 def power():
     form = powerForm()
 
-    # Detting the default value of alpha as 0.05
-    # if request.method == 'GET':
-    #     form.alpha.default = 0.05
-    #     form.process()
-
-
-    # if (request.method == 'POST'):
-    #     effect = request.form['effect']
-    #     nobs = request.form['nobs']
-    #     alpha = request.form['alpha']
-    #     power = request.form['power']
-    #     test = request.form['test']
-
-        # output the user input on the shell
-        # print("\nEffect: " + effect   + "    nobs: " + nobs  + "    Alpha: " +  alpha  + "     Power: " + power  + "   Test: " + test +"\n")
-
-        # output the user input on the webpage
-        # flash("\nEffect: " + effect  + "  |  " + " nobs: " + nobs + "  |  " + " Alpha: " +  alpha + "  |  " + " Power: " + power + "  |  " + " Test: " + test +"\n")
-        # powerFunc(effect, alpha, power, nobs, test)
-
-
-    # if (form.validate_on_submit()):
-    #     flash(' Effect: ' + effect + '\n Alpha: ' + alpha + '\n Power: ' + power + '\n Test: ' + test)
-    #     flash('Alpha' + alpha)
-    #     flash('Power ' + power)
-    #     flash('Test' + test)
-
     return render_template('power.html', form=form)
-
 
 '''
 # https://pythonspot.com/flask-web-forms/
 # https://www.reddit.com/r/flask/comments/a5hxdi/how_to_give_a_drop_down_list_a_selected_value/
 '''
-
-
 
 @app_biodi.route('/effectCalc', methods=['GET', 'POST'])
 def effectCalc():
@@ -80,21 +51,12 @@ def effectCalc():
         test = request.form['test']
         alternative = request.form['alternative']
 
-        
-
-
-        # # output the user input on the shell
+        # output the user input on the shell
         print("\nnobs: " + nobs  + "    Alpha: " +  alpha  + "     Power: " + power  + "   Test: " + test + "   Alternative: " + alternative + "\n")
         
         nobs = int(nobs)
         alpha = float(alpha)
         power = float(power)
-
-        # print(type(effect))
-        # print(type(nobs))
-        # print(type(alpha))
-        # print(type(power))
-        # print(type(test))
 
         # nobs = 5
         # alpha = 0.05
@@ -106,14 +68,8 @@ def effectCalc():
         elif (test == 'paired'):
             result = TTestIndPower().solve_power(effect_size=effect, nobs1=nobs, alpha=alpha, power=power, alternative=alternative)
 
-        print("\nThe Effect size should be {}\n".format(str(result)))
-        flash("The Effect size should be {}".format(str(result)))
-
-
-        # # output the user input on the webpage
-        # flash("\nnobs: " + nobs + "  |  " + " Alpha: " +  alpha + "  |  " + " Power: " + power + "  |  " + " Test: " + test +"\n")
-
-        # powerFunc(effect, nobs, alpha, power, test)
+        print("\nThe Effect size should be %.2f\n" % result)
+        flash("The Effect size should be %.2f\n" % result)
 
     return render_template('powerForms/effectCalc.html', form=form)
 
@@ -144,24 +100,14 @@ def nobsCalc():
         alpha = float(alpha)
         power = float(power)
 
-        # print(type(effect))
-        # print(type(nobs))
-        # print(type(alpha))
-        # print(type(power))
-        # print(type(test))
-
         if (test == 'independent'):
             result = TTestPower().solve_power(effect_size=effect, nobs=nobs, alpha=alpha, power=power, alternative=alternative)
         elif (test == 'paired'):
             result = TTestIndPower().solve_power(effect_size=effect, nobs1=nobs, alpha=alpha, power=power, alternative=alternative)
 
-        print("\nThe Sample size should be {}\n".format(str(result)))
-        flash("The Sample size should be {}".format(str(result)))
+        print("\nThe Sample size should be {}\n".format(m.ceil(result)))
+        flash("The Sample size should be {}\n".format(m.ceil(result)))
 
-        # # output the user input on the webpage
-        # flash("\nEffect: " + effect + "  |  " + " Alpha: " +  alpha + "  |  " + " Power: " + power + "  |  " + " Test: " + test +"\n")
-
-        # powerFunc(float(effect), nobs, float(alpha), float(power), test)
 
     return render_template('powerForms/nobsCalc.html', form=form)
 
@@ -192,26 +138,13 @@ def powerCalc():
         nobs = float(nobs)
         alpha = float(alpha)
 
-        # print(type(effect))
-        # print(type(nobs))
-        # print(type(alpha))
-        # print(type(power))
-        # print(type(test))
-
         if (test == 'independent'):
             result = TTestPower().solve_power(effect_size=effect, nobs=nobs, alpha=alpha, power=power, alternative=alternative)
         elif (test == 'paired'):
             result = TTestIndPower().solve_power(effect_size=effect, nobs1=nobs, alpha=alpha, power=power, alternative=alternative)
 
-        print("\nThe Power should be {}\n".format(str(result)))
-        flash("The Power should be {}".format(str(result)))
-
-        # # output the user input on the webpage
-        # flash("\nEffect: " + effect  + "  |  " + " nobs: " + nobs + "  |  " + " Alpha: " +  alpha + "  |  " + " Test: " + test +"\n")
-
-        # powerFunc(float(effect), int(nobs), float(alpha), power, test)
-
-        
+        print("\nThe Power should be %.2f\n" % result)
+        flash("The Power should be %.2f\n" % result)        
 
     return render_template('powerForms/powerCalc.html', form=form)
 
