406e8ebb97e2d8b256f1f6182f4057518a7f5ffa
mdctleo <mdctleo@hotmail.com>
2019-06-05 09:46:17 -0700

    look at meeeeeegit statusgit status!

---
+++ frontend/src/views/BiodiCsv/BiodiCsvDownloadView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <el-alert
+                v-show="error"
+                :title="error"
+                type="error"
+                show-icon
+                @close="error = null">
+        </el-alert>
+        <el-row>
+            <BiodiCsvDownload :metas="this.metas" @download="downloadBiodiCsvs"></BiodiCsvDownload>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BiodiCsvDownload"
+    }
+</script>
+
+<style scoped>
+
+</style>
