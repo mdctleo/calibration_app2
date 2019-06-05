ea46097c08353e9719a0680600ff4ea14a9f9bba
mdctleo <mdctleo@hotmail.com>
2019-06-05 11:09:26 -0700

    api git issue

---
+++ frontend/src/api/api.js
@@ -0,0 +1,126 @@
+const axios = require('axios');
+const BASE_URL = " http://localhost:5000";
+
+
+// Calibration
+
+export const getCalibrationFactors = async (model, isotope) => {
+    let endpoint = BASE_URL + "/calibrations";
+    return await axios.get(endpoint, {
+        params: {
+            model: model,
+            isotope: isotope
+        }
+    })
+};
+
+export const getCounters = async () => {
+    let endpoint = BASE_URL + "/counters";
+    return await axios.get(endpoint);
+};
+
+export const getIsotopes = async () => {
+    let endpoint = BASE_URL + "/isotopes";
+    return await axios.get(endpoint);
+};
+
+export const getCalibrationFactorsGraph = async (model, isotope) => {
+    let endpoint = BASE_URL + "/calibrations/graph";
+    return await axios.get(endpoint, {
+        params: {
+            model: model,
+            isotope: isotope
+        }
+    })
+};
+
+// BiodiCsv
+
+export const postBiodiCsvFiles = async (filesJson) => {
+    let endpoint = BASE_URL + "/biodicsv";
+    return await axios.post(endpoint, {
+        files: filesJson
+    })
+};
+
+export const getBiodiCsvMetas = async () => {
+    let endpoint = BASE_URL + "/biodicsv-metas";
+    return await axios.get(endpoint);
+};
+
+export const getBiodiCsv = async (id) => {
+    let endpoint = BASE_URL + "/biodicsv";
+    return await axios.get(endpoint, {
+        responseType: 'blob',
+        params: {
+            id: id
+        }
+    })
+};
+
+// Statistics
+
+export const calculateEffect = async (statisticForm) => {
+    let endpoint = BASE_URL + "/effectCalc";
+    return await axios.get(endpoint, {
+        params: {
+            nobs: statisticForm.nobs,
+            power: statisticForm.power,
+            alpha: statisticForm.alpha,
+            test: statisticForm.test,
+            alternative: statisticForm.alternative
+        }
+    })
+};
+
+export const calculateNobs = async (statisticForm) => {
+    let endpoint = BASE_URL + "/nobsCalc";
+    return await axios.get(endpoint, {
+        params: {
+            effect: statisticForm.effect,
+            power: statisticForm.power,
+            alpha: statisticForm.alpha,
+            test: statisticForm.test,
+            alternative: statisticForm.alternative
+        }
+    })
+};
+
+export const calculatePower = async (statisticForm) => {
+    let endpoint = BASE_URL + "/powerCalc";
+    return await axios.get(endpoint, {
+        params: {
+            effect: statisticForm.effect,
+            nobs: statisticForm.nobs,
+            alpha: statisticForm.alpha,
+            test: statisticForm.test,
+            alternative: statisticForm.alternative
+        }
+    })
+};
+
+export const getPowerGraph = async (statisticForm) => {
+    let endpoint = BASE_URL + "/powerCalcGraph";
+    return await axios.get(endpoint, {
+        params: {
+            effect: statisticForm.effect,
+            nobs: statisticForm.nobs,
+            alpha: statisticForm.alpha,
+            test: statisticForm.test,
+            alternative: statisticForm.alternative
+        }
+    })
+};
+
+export const getPowerTable = async (statisticForm) => {
+    let endpoint = BASE_URL + "/powerCalcTable";
+    return await axios.get(endpoint, {
+        params: {
+            effect: statisticForm.effect,
+            nobs: statisticForm.nobs,
+            alpha: statisticForm.alpha,
+            test: statisticForm.test,
+            alternative: statisticForm.alternative
+        }
+    })
+};
--- frontend/src/views/BiodiCsv/BiodiCsvDownloadView.vue
+++ frontend/src/views/BiodiCsv/BiodiCsvDownloadView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div v-loading="loading">
         <el-alert
                 v-show="error"
                 :title="error"
@@ -8,14 +8,34 @@
                 @close="error = null">
         </el-alert>
         <el-row>
-            <BiodiCsvDownload :metas="this.metas" @download="downloadBiodiCsvs"></BiodiCsvDownload>
+            <BiodiCsvDownload :metas="this.metas" @download="this.downloadBiodiCsv()"></BiodiCsvDownload>
         </el-row>
     </div>
 </template>
 
 <script>
+    import {mapActions, mapGetters} from 'vuex';
+    import * as types from '../../store/modules/BiodiCsv/types.js'
     export default {
-        name: "BiodiCsvDownload"
+        name: "BiodiCsvDownload",
+        computed: {
+            ...mapGetters({
+                metas: 'metas',
+                error: 'biodiCsvDownloadError',
+                loading: 'biodiCsvDownloadLoading'
+            })
+        },
+        methods: {
+            ...mapActions([
+                types.DOWNLOAD_BIODI_CSV,
+                types.GET_BIODI_CSV_METAS
+            ])
+
+        },
+
+        created() {
+            this.getBiodiCsvMetas()
+        }
     }
 </script>
 
